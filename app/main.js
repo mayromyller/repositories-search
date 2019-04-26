0ef27d7bedadef7bbda5a182ed95761c59f84980
Mayro Myller <deehmyller11@gmail.com>
2019-04-26 12:02:39 -0300

    added method asynchronous in search repositories

--- app/main.js
+++ app/main.js
@@ -63,25 +63,22 @@ const Data = (function($){
   const searchInput = document.querySelector('#search');
   searchInput.addEventListener('keyup', search);
 
-  function search(event){
+  // function search(event){
+  //   if(event && event.keyCode === 13){
+  //     const searchQuery = searchInput.value;
+  //     fetch(`https://api.github.com/search/repositories?q=${searchQuery}`)
+  //     .then(res=> res.json())
+  //     .then(res=>res.items)
+  //     .then($.setList);
+  //   }
+  // }
+
+  async function search(event){
     if(event && event.keyCode === 13){
-      $.setList([
-        {
-          full_name: 'Javascript',
-          created_at: '2020-07-25T20:10:50Z',
-          forks: 15300
-        },
-        {
-          full_name: 'PHP 7',
-          created_at: '2020-07-25T20:10:50Z',
-          forks: 2123
-        },
-        {
-          full_name: 'VueJs',
-          created_at: '2020-07-25T20:10:50Z',
-          forks: 256
-        }
-      ])
+      const searchQuery = searchInput.value;
+      let res = await fetch(`https://api.github.com/search/repositories?q=${searchQuery}`)
+      res = await res.json();
+      $.setList(res.items);
     }
   }
 })(Template);
